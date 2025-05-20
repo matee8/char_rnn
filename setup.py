52a3178c4a0f81e44e0c40cffe68918eb0de017f
matee8 <graves-bluff-pesky@duck.com>
2025-05-21 00:40:19 +0200

    docs: update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 NAME = "char_rnn"
 VERSION = "0.1.0"
-DESCRIPTION = "A character-level Recurrent Neural Network implementation."
+DESCRIPTION = "A character-level recurrent neural network implementation."
 AUTHOR = "matee8"
 AUTHOR_EMAIL = "graves-bluff-pesky@duck.com"
 URL = "https://github.com/matee8/char_rnn"
